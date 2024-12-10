31ae2d4f3e7ed0f1d00766446c237e7d6d660243
DvirAgassi <agassidvir@gmail.com>
2024-12-10 17:58:26 +0200

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -34,9 +34,21 @@ void printMenu(){
            "7.exit\n");
 }
 
+void initialize_minus_one(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]);
+
+int max(int a, int b);
+
+void add_one(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]);
+
+void add_all(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]);
+
+int added_all(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]);
+
+int next_available_day = 0;
 
 int main() {
     int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES];
+    initialize_minus_one(cube); // use the function to make all values in the array to -1
     int days[NUM_OF_BRANDS] = {0};
     int choice;
     printMenu();
@@ -44,14 +56,21 @@ int main() {
     while(choice != done){
         switch(choice){
             case addOne:
-                //...
+                add_one(days, cube);
                 break;
             case addAll:
-                //...
+                add_all(days, cube);
                 break;
-            /*
-             ......
-             */
+            case stats:
+                break;
+            case print:
+                break;
+            case insights:
+                break;
+            case deltas:
+                break;
+            case done:
+                break;
             default:
                 printf("Invalid input\n");
         }
@@ -62,4 +81,80 @@ int main() {
     return 0;
 }
 
+
+void initialize_minus_one(int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
+  for (int i = 0; i < DAYS_IN_YEAR; i++) {
+      for (int j = 0; j < NUM_OF_BRANDS; j++) {
+        for (int k = 0; k < NUM_OF_TYPES; k++) {
+          cube[i][j][k] = -1;
+        }
+      }
+    }
+}
+
+// find the max num func
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+void add_one(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
+    int brand_name;
+    int types[NUM_OF_TYPES];
+    int result = scanf("%d %d %d %d %d", &brand_name, &types[0], &types[1], &types[2], &types[3]);
+    while (result != 5 || (brand_name < 0 || brand_name > NUM_OF_BRANDS)) {
+      while(getchar() != '\n');
+      printf("This brand is not valid\n");
+      result = scanf("%d %d %d %d %d", &brand_name, &types[0], &types[1], &types[2], &types[3]);
+    }
+    
+    int type_place = 0; // if you entered a brand it means you have a type as well, so just a random type to see if the place is not -1
+    // check if there is already info in this day for this brand
+    if (cube[next_available_day][brand_name][type_place] != -1) {
+      printf("This brand is not valid\n");
+    }
+    else {
+      days[brand_name] = next_available_day;
+      for (int i = 0; i < NUM_OF_TYPES; i++) {
+        cube[next_available_day][brand_name][i] = types[i];
+      }
+    }
+}
+
+void add_all(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
+  int type_place = 0; // if you entered a brand it means you have a type as well, so just a random type to see if the place is not -1
+  while (added_all(days, cube)) {
+    printf("No data for brands ");
+    for (int i = 0; i < NUM_OF_BRANDS; i++) {
+      if (cube[next_available_day][i][type_place] == -1)
+        printf("%s ", brands[i]);
+    }
+    printf("\nPlease complete the data\n");
+    
+    add_one(days, cube);
+  }
+  next_available_day++;
+}
+
+int added_all(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
+  // check if all the brands in next_available_day have info inside to see if you put everything already
+  int all_added = 0;
+  int type_place = 0; // if you entered a brand it means you have a type as well, so just a random type to see if the place is not -1
+  for (int i = 0; i < NUM_OF_BRANDS; i++) {
+    if (cube[next_available_day][i][type_place] == -1)
+      break;
+    // it will get inside only if all the brands are not -1 on that day anymore
+    if (i + 1 == NUM_OF_BRANDS && cube[next_available_day][i][type_place] != -1) {
+      all_added = 1;
+    }
+  }
+}
+
+void stats(int days[NUM_OF_BRANDS], int cube[DAYS_IN_YEAR][NUM_OF_BRANDS][NUM_OF_TYPES]) {
+  printf("What day would you like to analyze?");
+  int day;
+  scanf("%d", &day);
+  while (!(day >= 0 && < 365)) {
+    printf("Please enter a valid day.\nWhich day would you like to analyze?\n");
+  }
+}
 
